4fc35ce358cd209e5008e23a63b7e5e0a42ceee2
GooberCoder <vaino.o.hanninen@gmail.com>
2026-04-21 18:13:15 +0300

    Added proper physics to the ball and player + added walls to prevent the ball leaving the game area. Player can still go out of bounds, but oh well.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,12 +3,35 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
 
-    public float startSpeed = 6f;
+    public float speed = 6f;
+
+    public Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
+    void LaunchBall()
+    {
+        rb.linearVelocity = Vector2.zero;
+
+        Vector2 startDir = new Vector2(Random.Range(-0.7f, 0.7f), 1f);
+        rb.linearVelocity = startDir * speed;
+    }
+
+    void Update()
+    {
+        if (gameObject.transform.position.y <= -5)
+        {
+            Debug.Log("Folk😭😭😭😭😭😭😭");
+        }
+    }
 
 }
